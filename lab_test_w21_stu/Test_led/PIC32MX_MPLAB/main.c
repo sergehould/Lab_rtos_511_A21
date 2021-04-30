1b4a22bbc63d57a3ee01a690317d25e812d140a3
sergehould <sergehould@yahoo.ca>
2021-04-30 10:28:17 -0400

    Signed-off-by: sergehould <sergehould@yahoo.ca>

---
+++ lab_test_w21_stu/Test_led/PIC32MX_MPLAB/main.c
@@ -0,0 +1,58 @@
+  
+/* ***************************************************************************
+ * File: main.c
+ * Description: Main source file for all Embedded Operating Systems labs.
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Author        	Date      		Comments on this revision
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * 
+ *                            
+ *****************************************************************************/
+
+/* Kernel includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h" 
+#include "semphr.h"
+#include "croutine.h"
+/* Hardware specific includes. */
+#include "include/ConfigPerformance.h"
+#include "include/console32.h"
+#include "include/public.h"
+
+/* Prototypes for the standard FreeRTOS callback/hook functions implemented within this file. */
+void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName );
+
+int main( void )
+{
+    /* Prepares the hardware */
+	prvSetupHardware();
+    //initIO();
+    //LCDInit();
+    //initUart1();
+    initUart2_wInt();
+    
+    //fprintf2(C_UART1, "Hi Uart1\n");
+    
+    /* create tasks here */
+    
+    vStartTask1();
+    vTaskStartScheduler();
+	return 0;
+}
+/*-----------------------------------------------------------*/
+
+
+void vApplicationIdleHook( void ){
+    while(1){
+        //your code here
+    }
+}
+
+void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName )
+{
+    pxTask= pxTask;
+    pcTaskName = pcTaskName;
+		for( ;; );
+}
+
