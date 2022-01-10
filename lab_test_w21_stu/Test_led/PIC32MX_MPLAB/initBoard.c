f77d50ba4dd7502557c49df89344a3d9d83e6786
sergehould <houlds@vaniercollege.qc.ca>
2022-01-10 16:04:40 -0500

    Update initBoard.c
    Updates fuses for SYSCLK = PBCLK = 80 MHz
    Updates initIOs()

--- lab_test_w21_stu/Test_led/PIC32MX_MPLAB/initBoard.c
+++ lab_test_w21_stu/Test_led/PIC32MX_MPLAB/initBoard.c
@@ -17,24 +17,39 @@
  // Configuration Bit settings
 // SYSCLK = 80 MHz 
 //(8MHz Crystal/ FPLLIDIV * FPLLMUL / FPLLODIV)
-// PBCLK = 40 MHz
+// PBCLK = 80 MHz
 // Primary Osc w/PLL (HS+PLL)
-// WDT OFF, Peripheral Bus is CPU clock÷8
+// WDT OFF, Peripheral Bus is CPU clockÃ·8
 // Other options are default as per datasheet
 // see file:///C:/Program%20Files/Microchip/xc32/v2.41/docs/config_docs/32mx795f512l.html
  
-
 #pragma config FPLLMUL = MUL_20
 #pragma config FPLLIDIV = DIV_2
-#pragma config FPLLODIV = DIV_1, FWDTEN = OFF
+#pragma config FPLLODIV = DIV_1 
 #pragma config POSCMOD = HS, FNOSC = PRIPLL
-#pragma config FPBDIV = DIV_2   // PBCLK = SYSCLK/DIV_2
+#pragma config FPBDIV = DIV_1  // PBCLK = SYSCLK/DIV_1
+#pragma config FWDTEN =  OFF    // disable
 
 void initIO(void)
 {
+    DDPCONbits.JTAGEN = 0; // JTAG shared with RA5, RA4, RA1 and RA0
     /* LEDs */
-    TRISAbits.TRISA0 = 0; // LED D3
-    TRISAbits.TRISA1 = 0; // LED D4
+    TRISAbits.TRISA7 = 0;       //LED D10
+    TRISAbits.TRISA5 = 0;       //LED D8
+    TRISAbits.TRISA4 = 0;       //LED D7
+    TRISAbits.TRISA3 = 0;       //LED D6
+    TRISAbits.TRISA2 = 0;       //LED D5
+    TRISAbits.TRISA1 = 0;       //LED D4
+    TRISAbits.TRISA0 = 0;       //LED D3
+    
+   
+    TRISDbits.TRISD6 =1; //S3
+    TRISDbits.TRISD7 =1;//S6
+    TRISDbits.TRISD13 =1;//S4
+    TRISAbits.TRISA6 =1; //S5 - shared with LED D9
+	
+    /* Turns off all LEDs*/
+     LATA = LATA & 0xffff0000;
   
 }
 
